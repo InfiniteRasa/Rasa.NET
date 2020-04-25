592cb024d0c9965779f6f678d449d2485b70ab11
krssrb <ivankohodolic@gmail.com>
2020-04-25 11:22:25 +0200

    Character got new look

--- src/Rasa.Game/Managers/MapManager.cs
+++ src/Rasa.Game/Managers/MapManager.cs
@@ -1,9 +1,12 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Rasa.Managers
 {
     using Data;
+    using Database.Tables.Character;
     using Game;
+    using Packets;
     using Packets.Game.Server;
     using Structures;
     using Timer;
@@ -41,15 +44,23 @@ namespace Rasa.Managers
         {
             var player = client.MapClient.Player;
 
+            // default enetiyId, until we implement EntityManager
+            var entityId = 10001u;
+
+            var entityData = new List<PythonPacket>
+            {
+                new WorldLocationDescriptorPacket(new Vector3((float)player.CoordX, (float)player.CoordY, (float)player.CoordZ), (float)player.Rotation),
+                new AppearanceDataPacket(CharacterAppearanceTable.GetAppearances(player.Id))
+            };
+
             var map = new Map
             {
                 MapInfo = new MapInfo(1220, "adv_foreas_concordia_wilderness", 1556, 0)
             };
 
-            client.CallMethod(SysEntity.ClientMethodId, new CreatePhysicalEntityPacket(10001, (EntityClass)player.Gender == 0 ? (EntityClass)692 : (EntityClass)691));
-            client.CallMethod(10001, new WorldLocationDescriptorPacket(new Vector3((float)player.CoordX, (float)player.CoordY, (float)player.CoordZ), (float)player.Rotation));
+            client.CallMethod(SysEntity.ClientMethodId, new CreatePhysicalEntityPacket(entityId, (EntityClass)player.Gender == 0 ? (EntityClass)692 : (EntityClass)691, entityData));
             client.CallMethod(SysEntity.ClientMethodId, new SetCurrentContextIdPacket(map.MapInfo.ContextId));
-            client.CallMethod(SysEntity.ClientMethodId, new SetControlledActorIdPacket(10001));
+            client.CallMethod(SysEntity.ClientMethodId, new SetControlledActorIdPacket(entityId));
 
             client.State = ClientState.Ingame;
         }
---
+++ src/Rasa.Game/Packets/Game/Server/AppearanceDataPacket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Rasa.Packets.Game.Server
+{
+    using Data;
+    using Memory;
+    using Structures;
+
+    public class AppearanceDataPacket : ServerPythonPacket
+    {
+        public override GameOpcode Opcode { get; } = GameOpcode.AppearanceData;
+
+        public Dictionary<uint, CharacterAppearanceEntry> AppearanceData { get; set; }
+
+        public AppearanceDataPacket(Dictionary<uint, CharacterAppearanceEntry> appearanceData)
+        {
+            AppearanceData = appearanceData;
+        }
+
+        public override void Write(PythonWriter pw)
+        {
+            pw.WriteTuple(1);
+            pw.WriteDictionary(AppearanceData.Count);
+            foreach (var t in AppearanceData)
+            {
+                var appearance = t.Value;
+
+                pw.WriteUInt(appearance.Slot);
+                pw.WriteTuple(2);
+                pw.WriteUInt(appearance.Class);
+                pw.WriteStruct(new Color(appearance.Color));
+            }
+        }
+    }
+}
