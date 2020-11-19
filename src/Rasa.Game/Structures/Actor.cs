30500bf762953f34910333e3f7f324237a139f6c
Tobias Häusler <github@tobias-haeusler.net>
2020-11-19 19:08:43 +0100

    Just refactoring, no functional change (#56)
    * fixed non updated timers of game server
    
    * fixed game server to auth server reconnection
    
    * fixed on error call
    
    * * code style
    * added shared DotSettings with settings that should match the current code style
    
    * some code refactorings without any functional change
    
    * - updated namespace usings to be nested
    - updated resharper settings to prefer nested usings
    
    Co-authored-by: th <th@tobias-haeusler.net>

---
+++ src/Rasa.Game/Structures/Actor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Rasa.Structures
+{
+    using Data;
+    using Managers;
+    using Positioning;
+
+    public class Actor : IHasPosition
+    {
+        public Actor()
+        {
+            EntityId = EntityManager.Instance.GetEntityId;
+        }
+        public uint EntityId { get; set; }
+        public EntityClassId EntityClassId { get; set; }
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public Vector3 Position { get; set; }
+        public float Orientation { get; set; }
+        public uint MapContextId { get; set; }
+        public bool IsRunning { get; set; }
+        public bool InCombatMode { get; set; }
+        public ActorState State { get; set; }
+        public bool WeaponReady { get; set; }
+        // action data
+        public int CurrentAction { get; set; }
+        public Dictionary<Attributes, ActorAttributes> Attributes = new Dictionary<Attributes, ActorAttributes>();
+        public Dictionary<int, GameEffect> ActiveEffects { get; set; } = new Dictionary<int, GameEffect>();
+        public uint[,] Cells = new uint[5 ,5];
+        // sometimes we only have access to the actor, the owner variable allows us to access the client anyway (only if actor is a player manifestation)
+        //public MapChannelClient Owner { get; set; }
+    }
+}
