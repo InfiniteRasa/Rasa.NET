e83cd72acc4ace4eb40d2950a3f3830d896019ee
th <th@tobias-haeusler.net>
2020-12-11 00:07:22 +0100

    migrate to EF Core * move database configuration to database project * database configuration structure instead of connection string

---
+++ src/Rasa.DBL/Configuration/DatabaseConnectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Rasa.Configuration
+{
+    public class DatabaseConnectionConfiguration
+    {
+        public string Provider { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public int TimeoutInMilliseconds { get; set; }
+    }
+}
