70837a33d58e80bee1b8e55f6630e052ac6e2517
th <th@tobias-haeusler.net>
2020-12-10 13:32:26 +0100

    migrate to EF Core * update AuthAccountEntry to be EF core compliant

--- src/Rasa.DBL/Structures/AuthAccountEntry.cs
+++ src/Rasa.DBL/Structures/AuthAccountEntry.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using MySql.Data.MySqlClient;
 
 namespace Rasa.Structures
 {
+    using Microsoft.EntityFrameworkCore;
+
+    [Table(AuthAccountEntry.TableName)]
+    [Index(nameof(AuthAccountEntry.Email), IsUnique = true, Name = "email_UNIQUE")]
+    [Index(nameof(AuthAccountEntry.Username), IsUnique = true, Name = "email_UNIQUE")]
     public class AuthAccountEntry
     {
+        public const string TableName = "account";
+
+        [Key]
+        [Column(TypeName = "int(11)")]
         public uint Id { get; set; }
+
+        [EmailAddress]
+        [Column(TypeName = "varchar(255)")]
         public string Email { get; set; }
+
+        [Column(TypeName = "varchar(64)")]
         public string Username { get; set; }
+
+        [Column(TypeName = "varchar(64)")]
         public string Password { get; set; }
+
+        [Column(TypeName = "varchar(40)")]
         public string Salt { get; set; }
+
+        [Column(TypeName = "tinyint(3)")]
         public byte Level { get; set; }
+
+        [Column("last_ip", TypeName = "varchar(45)")]
+        public string LastIp { get; set; }
+
+        [Column("last_server_id", TypeName = "tinyint(3)")]
         public byte LastServerId { get; set; }
+
         public bool Locked { get; set; }
+
         public bool Validated { get; set; }
 
         public static AuthAccountEntry Read(MySqlDataReader reader)
