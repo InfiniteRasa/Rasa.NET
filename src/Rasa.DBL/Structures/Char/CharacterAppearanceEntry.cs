ede1f0d2b1cbda771e3a36b5207d1aa5508c52b9
th <th@tobias-haeusler.net>
2020-12-18 21:10:59 +0100

    create/delete char * added additional default appearance

--- src/Rasa.DBL/Structures/Char/CharacterAppearanceEntry.cs
+++ src/Rasa.DBL/Structures/Char/CharacterAppearanceEntry.cs
@@ -2,10 +2,21 @@
 
 namespace Rasa.Structures.Char
 {
-    [Table(CharacterAppearanceEntry.TableName)]
+    [Table(TableName)]
     public class CharacterAppearanceEntry
     {
         public const string TableName = "character_appearance";
+
+        public CharacterAppearanceEntry()
+        {
+        }
+
+        public CharacterAppearanceEntry(uint slot, uint classId, uint color)
+        {
+            Slot = slot;
+            Class = classId;
+            Color = color;
+        }
 
         [Column("character_id")]
         public uint CharacterId { get; set; }
