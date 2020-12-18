a5f47f0b8a4dfb4295c2b7fddcf0c95273fd872b
th <th@tobias-haeusler.net>
2020-12-18 01:04:27 +0100

    migrate world database to EF Core * moved MySqlChar migrations to dedicated namespace

--- src/Rasa.DBL/Migrations/MySqlChar/MySqlCharContextModelSnapshot.cs
+++ src/Rasa.DBL/Migrations/MySqlChar/MySqlCharContextModelSnapshot.cs
@@ -3,17 +3,15 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rasa.Context.Char;
 
-namespace Rasa.Migrations
+namespace Rasa.Migrations.MySqlChar
 {
     [DbContext(typeof(MySqlCharContext))]
-    [Migration("20201217152216_Initial")]
-    partial class Initial
+    partial class MySqlCharContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
