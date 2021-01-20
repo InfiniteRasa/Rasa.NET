c7a5e204ffb6cba2a5336c836f5e5fde0ee39ade
infiniterasadev <47341685+infiniterasadev@users.noreply.github.com>
2021-01-19 22:19:55 -0500

    Adds additional manifestation client packets

---
+++ src/Rasa.Game/Packets/Game/Server/LogoutTimeRemainingPacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rasa.Packets.Game.Server
+{
+    using Data;
+    using Memory;
+
+    public class LogoutTimeRemainingPacket : ServerPythonPacket
+    {
+        public override GameOpcode Opcode { get; } = GameOpcode.LogoutTimeRemaining;
+
+        public int LogoutType { get; set; }
+
+        public override void Write(PythonWriter pw)
+        {
+            pw.WriteTuple(1);
+            pw.WriteInt(5000); // 5 sec
+        }
+    }
+}
