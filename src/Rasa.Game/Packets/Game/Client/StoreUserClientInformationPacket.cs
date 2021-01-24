b78e0b72a153918c74b2b8a86143215d9c28153f
Blumster <sarkany0@hotmail.com>
2021-01-24 05:04:13 +0100

    Add StoreUserClientInformationPacket class

---
+++ src/Rasa.Game/Packets/Game/Client/StoreUserClientInformationPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Rasa.Packets.Game.Client
+{
+    using Data;
+    using Memory;
+
+    public class StoreUserClientInformationPacket : ClientPythonPacket
+    {
+        public override GameOpcode Opcode { get; } = GameOpcode.StoreUserClientInformation;
+
+        public override void Read(PythonReader pr)
+        {
+            pr.ReadTuple();
+            pr.ReadString();
+            pr.ReadString();
+        }
+    }
+}
