52107daaf0553c8570ac22d39931d8c3d90ca75f
th <th@tobias-haeusler.net>
2021-01-25 02:16:02 +0100

    process SetDesiredCrouchState

--- src/Rasa.Game/Structures/Manifestation.cs
+++ src/Rasa.Game/Structures/Manifestation.cs
@@ -1,13 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Rasa.Structures
 {
+    using Data;
     using Structures.Char;
 
     public class Manifestation : Actor
     {
         public uint Gender { get; set; }
+
         public bool IsRunning { get; set; }
+
+        public bool IsCrouching { get; set; }
+
+        public void SetIsCrouching(Posture posture)
+        {
+            switch (posture)
+            {
+                case Posture.Crouching:
+                    IsCrouching = true;
+                    break;
+                case Posture.Standing:
+                    IsCrouching = false;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(posture), posture, null);
+            }
+        }
+
+        public Posture GetPosture()
+        {
+            return IsCrouching
+                ? Posture.Crouching
+                : Posture.Standing;
+        }
+
         public uint MapContextId { get; set; }
         public List<CharacterAppearanceEntry> AppearanceData { get; set; }
     }
